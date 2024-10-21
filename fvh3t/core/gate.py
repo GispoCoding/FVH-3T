c8d23a202a339a0d460a5cdc15c32f0c727ac91e
Juho Ervasti <juho.ervasti@gispo.fi>
2024-10-21 09:45:31 +0300

    Add TrajectoryLayer and docstrings

--- fvh3t/core/gate.py
+++ fvh3t/core/gate.py
@@ -1,6 +1,11 @@
 from qgis.core import QgsGeometry
 
 class Gate:
+    """
+    A wrapper class around a QgsGeometry which represents a
+    gate through which trajectories can pass. The geometry
+    must be a line.
+    """
 
     def __init__(self, geom: QgsGeometry) -> None:
         self.__geom: QgsGeometry = geom
--- fvh3t/core/trajectory.py
+++ fvh3t/core/trajectory.py
@@ -1,10 +1,15 @@
 from typing import Tuple, NamedTuple
 
-from qgis.core import QgsGeometry, QgsPointXY
+from qgis.core import QgsGeometry, QgsPointXY, QgsVectorLayer, QgsWkbTypes
 
 from fvh3t.core.gate import Gate
 
 class TrajectoryNode(NamedTuple):
+    """
+    A simple data container representing one node in a
+    trajectory.
+    """
+
     point: QgsPointXY
     timestamp: int
 
@@ -14,6 +19,10 @@ class TrajectoryNode(NamedTuple):
 
 
 class Trajectory:
+    """
+    Class representing a trajectory which consists
+    of nodes which have a location and a timestamp
+    """
 
     def __init__(self, nodes: Tuple[TrajectoryNode]) -> None:
         self.__nodes: Tuple[TrajectoryNode] = nodes
@@ -28,3 +37,25 @@ class Trajectory:
         # TODO: implement function
         return 0.0
 
+
+class TrajectoryLayer:
+    """
+    Wrapper around a QgsVectorLayer object from which trajectories
+    can be instantiated, i.e.
+
+    1. is a point layer
+    2. has a valid identifier field
+    3. has a valid timestamp field
+    """
+
+    def __init__(self, layer: QgsVectorLayer, id_field: str, timestamp_field: str) -> None:
+        self.__layer: QgsVectorLayer = layer
+        self.__id_field: str = id_field
+        self.__timestamp_field: str = timestamp_field
+
+    def is_valid(self) -> bool:
+        is_point_layer: bool = self.__layer.geometryType() == QgsWkbTypes.GeometryType.PointGeometry
+        id_field_exists: bool = self.__layer.fields().indexFromName(self.__id_field) != -1
+        timestamp_field_exists: bool = self.__layer.fields().indexFromName(self.__timestamp_field) != -1
+
+        return is_point_layer and id_field_exists and timestamp_field_exists
