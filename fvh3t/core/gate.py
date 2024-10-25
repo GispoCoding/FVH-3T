1fd06a760e4af625d88b5ece8ef394ab696f71a2
Juho Ervasti <juho.ervasti@gispo.fi>
2024-10-25 17:12:51 +0300

    Passes -> crosses, use crosses() not intersects()

--- fvh3t/core/gate.py
+++ fvh3t/core/gate.py
@@ -29,52 +29,64 @@ class GateSegment:
 
         self.__geom = QgsGeometry.fromPolylineXY([point_a, point_b])
 
-    def trajectory_segment_passes(self, traj_seg: TrajectorySegment, *, counts_left: bool, counts_right: bool) -> bool:
-        intersects = self.__geom.intersects(traj_seg.as_geometry())
+    def trajectory_segment_crosses(self, traj_seg: TrajectorySegment, *, counts_left: bool, counts_right: bool) -> bool:
+        crosses = self.__geom.crosses(traj_seg.as_geometry())
 
-        if not intersects:
+        if not crosses:
+            if self.__geom.intersects(traj_seg.as_geometry()):
+                # TODO: Handle case where the segment doesn't cross this segment
+                # but instead intersects it. In practice this means that when a trajectory
+                # has a vertex exactly on this segment we cannot rely on calculating
+                # the crossing direction from the singular segment.
+                # Realistically the chances of a trajectory node landing exactly on the
+                # gate segment should be extremely rare, but regardless we do need
+                # to deal with that... BUT right now in the interest of moving things
+                # forwards I'm leaving this as a TODO
+                msg = "Cannot determine direction from trajectory segment!"
+                raise InvalidDirectionException(msg)
+
             return False
 
         if counts_left and counts_right:
             return True
 
-        passes_from: RelativeDirection = self._trajectory_segment_passes_from(traj_seg)
+        crosses_from: RelativeDirection = self._trajectory_segment_crosses_from(traj_seg)
 
-        if counts_left and passes_from == RelativeDirection.LEFT:
+        if counts_left and crosses_from == RelativeDirection.LEFT:
             return True
 
-        return counts_right and passes_from == RelativeDirection.RIGHT
+        return counts_right and crosses_from == RelativeDirection.RIGHT
 
-    def _trajectory_segment_passes_from(self, segment: TrajectorySegment) -> RelativeDirection:
+    def _trajectory_segment_crosses_from(self, segment: TrajectorySegment) -> RelativeDirection:
         node_a_dir: RelativeDirection = self.point_relative_direction(segment.node_a.point)
         node_b_dir: RelativeDirection = self.point_relative_direction(segment.node_b.point)
 
-        passes_from: RelativeDirection = RelativeDirection.UNKNOWN
+        crosses_from: RelativeDirection = RelativeDirection.UNKNOWN
 
         # node A should always be the first node, timewise
 
         if node_a_dir == RelativeDirection.LEFT and node_b_dir == RelativeDirection.RIGHT:
-            passes_from = RelativeDirection.LEFT
+            crosses_from = RelativeDirection.LEFT
         elif node_a_dir == RelativeDirection.RIGHT and node_b_dir == RelativeDirection.LEFT:
-            passes_from = RelativeDirection.RIGHT
+            crosses_from = RelativeDirection.RIGHT
         elif node_a_dir == RelativeDirection.COLLINEAR and node_b_dir == RelativeDirection.RIGHT:
-            passes_from = RelativeDirection.LEFT
+            crosses_from = RelativeDirection.LEFT
         elif (
             node_a_dir == RelativeDirection.COLLINEAR
             and node_b_dir == RelativeDirection.LEFT
             or node_a_dir == RelativeDirection.RIGHT
             and node_b_dir == RelativeDirection.COLLINEAR
         ):
-            passes_from = RelativeDirection.RIGHT
+            crosses_from = RelativeDirection.RIGHT
         elif node_a_dir == RelativeDirection.LEFT and node_b_dir == RelativeDirection.COLLINEAR:
-            passes_from = RelativeDirection.LEFT
+            crosses_from = RelativeDirection.LEFT
         elif node_a_dir == RelativeDirection.COLLINEAR and node_b_dir == RelativeDirection.COLLINEAR:
             # i.e. the trajectory segment is exactly
             # on this gate segment.
             # realistically this should be extremely rare,
             # but regardless we have to deal with this
             # edge case.
-            passes_from = RelativeDirection.COLLINEAR
+            crosses_from = RelativeDirection.COLLINEAR
         elif node_a_dir == node_b_dir:
             # it is assumed that it has already been checked
             # that the trajectory segment intersects
@@ -92,18 +104,17 @@ class GateSegment:
             msg = "Directions cannot be unkown!"
             raise InvalidDirectionException(msg)
 
-        return passes_from
+        return crosses_from
 
     def point_relative_direction(self, point: QgsPointXY) -> RelativeDirection:
-        pos: float = (
-            self.__point_b.x()
-            - self.__point_a.x() * (point.y() - self.__point_a.y())
-            - (self.__point_b.y() - self.__point_a.y()) * (point.x() - self.__point_a.x())
-        )
+        pos: float = (point.x() - self.__point_a.x()) * (self.__point_b.y() - self.__point_a.y()) - (
+            point.y() - self.__point_a.y()
+        ) * (self.__point_b.x() - self.__point_a.x())
+
         if pos > 0:
-            direction = RelativeDirection.LEFT
-        elif pos < 0:
             direction = RelativeDirection.RIGHT
+        elif pos < 0:
+            direction = RelativeDirection.LEFT
         else:
             direction = RelativeDirection.COLLINEAR
 
@@ -150,6 +161,9 @@ class Gate:
     def segments(self) -> tuple[GateSegment, ...]:
         return self.__segments
 
+    def crosses_trajectory(self, traj: Trajectory) -> bool:
+        return self.__geom.crosses(traj.as_geometry())
+
     def create_segments(self) -> None:
         segments: list[GateSegment] = []
         polyline: list[QgsPointXY] = self.__geom.asPolyline()
@@ -170,15 +184,15 @@ class Gate:
             # check if geometries intersect at all before
             # checking which specific segments intersect
             # to save time
-            if self.__geom.intersects(trajectory.as_geometry()):
+            if self.crosses_trajectory(trajectory):
                 for traj_seg in trajectory.as_segments():
                     for segment in self.__segments:
-                        # TODO: The case where a trajectory passes
+                        # TODO: The case where a trajectory crosses
                         # the same gate multiple times is not handled
-                        passes: bool = segment.trajectory_segment_passes(
+                        crosses: bool = segment.trajectory_segment_crosses(
                             traj_seg, counts_left=self.__counts_left, counts_right=self.__counts_right
                         )
-                        if passes:
+                        if crosses:
                             self.__trajectory_count += 1
 
 
