0aa0ab0f3451225f2b0c31cff858ea9f2423381e
Juho Ervasti <juho.ervasti@gispo.fi>
2024-11-01 14:41:44 +0200

    Simplify algorithm

--- fvh3t/fvh3t_processing/count_trajectories.py
+++ fvh3t/fvh3t_processing/count_trajectories.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from typing import Any
 
 from qgis.core import (
-    QgsFeature,
     QgsFeatureSink,
     QgsProcessing,
     QgsProcessingAlgorithm,
@@ -13,9 +12,8 @@ from qgis.core import (
     QgsProcessingParameterFeatureSink,
     QgsProcessingParameterVectorLayer,
     QgsUnitTypes,
-    QgsVectorLayer,
 )
-from qgis.PyQt.QtCore import QCoreApplication
+from qgis.PyQt.QtCore import QCoreApplication, QDateTime
 
 from fvh3t.core.gate_layer import GateLayer
 from fvh3t.core.trajectory_layer import TrajectoryLayer
@@ -111,8 +109,21 @@ class CountTrajectories(QgsProcessingAlgorithm):
             feedback = QgsProcessingFeedback()
 
         point_layer = self.parameterAsVectorLayer(parameters, self.INPUT_POINTS, context)
-        start_time = self.parameterAsDateTime(parameters, self.START_TIME, context)
-        end_time = self.parameterAsDateTime(parameters, self.END_TIME, context)
+        start_time: QDateTime = self.parameterAsDateTime(parameters, self.START_TIME, context)
+        end_time: QDateTime = self.parameterAsDateTime(parameters, self.END_TIME, context)
+
+        # TODO: Currently the datetime widget doesn't allow the user to set the seconds
+        # so normalize them to 0 seconds, because they are being set seemingly
+        # randomly leading to odd results. We need to check if selecting the time by
+        # minutes is OK and if not we might need to write a custom widget
+
+        zero_s_start_time = start_time.time()
+        zero_s_start_time.setHMS(zero_s_start_time.hour(), zero_s_start_time.minute(), 0)
+        start_time.setTime(zero_s_start_time)
+
+        zero_s_end_time = end_time.time()
+        zero_s_end_time.setHMS(zero_s_end_time.hour(), zero_s_end_time.minute(), 0)
+        end_time.setTime(zero_s_end_time)
 
         ## CREATE TRAJECTORIES
 
@@ -137,55 +148,20 @@ class CountTrajectories(QgsProcessingAlgorithm):
             msg = "Set start and/or end timestamps are out of data's range."
             raise ValueError(msg)
 
-        # Prepare a memory layer for filtered points
-        fields = point_layer.fields()
-        filtered_layer = QgsVectorLayer("Point?crs=" + point_layer.crs().authid(), "Filtered points", "memory")
-        filtered_layer.dataProvider().addAttributes(fields)
-        filtered_layer.updateFields()
-
-        id_count = {}
-        for feature in point_layer.getFeatures():
-            timestamp = feature["timestamp"]
-            feature_id = feature["id"]
-
-            # Filter features based on timestamp
-            if start_time_unix <= timestamp <= end_time_unix:
-                new_feature = QgsFeature(feature)
-                filtered_layer.dataProvider().addFeature(new_feature)
-
-                # Count ids
-                if feature_id not in id_count:
-                    id_count[feature_id] = 0
-                id_count[feature_id] += 1
-
-        feedback.pushInfo(f"Filtered {filtered_layer.featureCount()} features based on timestamp range.")
-
-        # Prepare another memory layer for features with non-unique id after filtering time
-        non_unique_layer = QgsVectorLayer(
-            "Point?crs=" + point_layer.crs().authid(), "Non-unique filtered points", "memory"
-        )
-        non_unique_layer.dataProvider().addAttributes(fields)
-        non_unique_layer.updateFields()
-
-        for feature in filtered_layer.getFeatures():
-            feature_id = feature["id"]
-            # Add only features with non-unique id
-            if id_count.get(feature_id, 0) > 1:
-                new_feature = QgsFeature(feature)
-                non_unique_layer.dataProvider().addFeature(new_feature)
-
-        feedback.pushInfo(
-            f"Final filtered point layer contains {non_unique_layer.featureCount()} features with non-unique IDs."
-        )
+        # If start or end time was given, filter the nodes outside the time range
+        filter_expression: str | None = None
+        if start_time_unix != min_timestamp or end_time_unix != max_timestamp:
+            filter_expression = f'"timestamp" BETWEEN {start_time_unix} AND {end_time_unix}'
 
         trajectory_layer = TrajectoryLayer(
-            non_unique_layer,
+            point_layer,
             "id",
             "timestamp",
             "size_x",
             "size_y",
             "size_z",
             QgsUnitTypes.TemporalUnit.TemporalMilliseconds,
+            filter_expression,
         )
 
         exported_traj_layer = trajectory_layer.as_line_layer()
@@ -236,10 +212,4 @@ class CountTrajectories(QgsProcessingAlgorithm):
         for feature in exported_gate_layer.getFeatures():
             sink.addFeature(feature, QgsFeatureSink.FastInsert)
 
-        # Return the results of the algorithm. In this case our only result is
-        # the feature sink which contains the processed features, but some
-        # algorithms may return multiple feature sinks, calculated numeric
-        # statistics, etc. These should all be included in the returned
-        # dictionary, with keys matching the feature corresponding parameter
-        # or output names.
         return {self.OUTPUT_TRAJECTORIES: traj_dest_id, self.OUTPUT_GATES: gate_dest_id}
